3331dfe24c3225078a7304bfa87a379e7c9611b4
Cata Yu <402169647@qq.com>
2024-08-04 17:55:18 +0800

    0

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('play-button').addEventListener('click', function() {
+    // 这里可以添加播放音频或视频的代码
+    // 例如播放一个音频文件
+    var audio = new Audio('media/test.wav');
+    audio.play();
+});
